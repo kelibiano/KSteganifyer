a60617e0d13c004813c9bc311daabe49485653f7
Yacine HAOUES <yacine.hs@gmail.com>
2017-11-05 22:37:24 +0100

    #4 add module management classes

---
+++ headers/Types.h
@@ -0,0 +1,40 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 yacinehaoues.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/* 
+ * File:   Types.h
+ * Author: yacinehaoues
+ *
+ * Created on November 4, 2017, 5:28 PM
+ */
+
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef std::string String;
+typedef std::vector<String> StringVector;
+typedef std::map Map;
+
+#endif /* TYPES_H */
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,59 @@
 
+/*
+ * The MIT License
+ *
+ * Copyright 2017 yacinehaoues.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+/*****************************************************+* Third party includes */
 #include <boost/program_options.hpp>
 #include <iostream>
 
-/* Program Options name space*/
+/***************************************************** Project party includes */
+#include <Module.h>
+
+
 using namespace boost::program_options;
+
+
+struct myclass {           // function object type:
+  void operator() (variables_map::value_type & i) {std::cout << i.first << std::endl;}
+} myobject;
+
 
 int main(int ac, char const *av[]) {
     options_description desc("Allowed options");
     desc.add_options()
             ("help", "produce help message")
-            ("compression", value<int>(), "set compression level");
+                ("compression", value<int>(), "set compression level");
 
     variables_map vm;
     store(parse_command_line(ac, av, desc), vm);
     notify(vm);
 
+    
+    std::for_each(vm.begin(), vm.end(), myobject);
+    std::cout << "params : " <<vm.size() << std::endl;
+    
     if (vm.count("compression")) {
         std::cout << "Compression level was set to "
                 << vm["compression"].as<int>() << ".\n";
@@ -25,3 +64,4 @@ int main(int ac, char const *av[]) {
     std::cout << "/* We are still Building ... */" << '\n';
     return 0;
 }
+
