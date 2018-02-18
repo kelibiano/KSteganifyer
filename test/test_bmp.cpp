bf8d3e273c73e67aa1b6c357bd68cc47ebb8b522
Yacine HAOUES <yacine.hs@gmail.com>
2018-02-18 19:52:42 +0100

    # add unit tests for coverage

--- test/test_bmp.cpp
+++ test/test_bmp.cpp
@@ -3,17 +3,60 @@ using namespace boost::unit_test;
 
 #include <Application.h>
 #include <ModulesManagers.h>
+#include <SimpleModule.h>
+#include <Command.h>
+
+void testHandler(const API::Command *const cmd, API::CommandContext *const ctx){}
+class TestModule : public API::SimpleModule {
+    public: TestModule() :SimpleModule("TestModule"){registerCommandHandler("test", testHandler);};
+};
 
 //____________________________________________________________________________//
+// APPLICATION TEST
+//____________________________________________________________________________//
 
-void test_case1() { 
+void test_Application() { 
     API::Application app;
     BOOST_CHECK(app.getExecutionResult() == 0);
     API::ModulesManager * mgr = app.getModulesManager();
     BOOST_CHECK(mgr != NULL);
+    app.start(0, NULL);
+    BOOST_CHECK(app.getExecutionResult() == 0);
 }
-void test_case2() { /* : */ }
-void test_case3() { /* : */ }
+
+//____________________________________________________________________________//
+// MODULES MANAGER TEST
+//____________________________________________________________________________//
+
+void test_ModulesManager() {
+    API::ModulesManager mgr;
+    TestModule module;
+    API::Command cmdTest("test");
+    API::Command cmdNotFound("notFound");
+    BOOST_CHECK(mgr.registerModule(&module));
+    BOOST_CHECK(mgr.getModuleForCommand(&cmdTest) == NULL);
+    BOOST_CHECK(mgr.getModuleForCommand(&cmdNotFound) == &module);
+    BOOST_CHECK(mgr.unregisterModule(&module));
+    BOOST_CHECK(mgr.getModuleForCommand(&cmdTest) == NULL);
+}
+
+//____________________________________________________________________________//
+// SIMLPE MODULE TEST
+//____________________________________________________________________________//
+
+struct TestCllBk : public API::Callback {
+    void operator()(API::Module * m){};
+};
+
+void test_Modules() { 
+    API::SimpleModule module("simple");
+    TestCllBk cll;
+    module.setCleanLibraryCallback(&cll);
+    API::Callback * cbk = module.getCleanCallback();
+    BOOST_CHECK(cbk == &cll);
+    cll(&module);
+}
+
 void test_case4() { /* : */ }
 
 //____________________________________________________________________________//
@@ -21,12 +64,12 @@ void test_case4() { /* : */ }
 test_suite*
 init_unit_test_suite(int argc, char* argv[])
 {
-    test_suite* ts1 = BOOST_TEST_SUITE("test_suite1");
-    ts1->add(BOOST_TEST_CASE(&test_case1));
-    ts1->add(BOOST_TEST_CASE(&test_case2));
+    test_suite* ts1 = BOOST_TEST_SUITE("SUITE_ApplicationNModules");
+    ts1->add(BOOST_TEST_CASE(&test_Application));
+    ts1->add(BOOST_TEST_CASE(&test_ModulesManager));
+    ts1->add(BOOST_TEST_CASE(&test_Modules));
 
-    test_suite* ts2 = BOOST_TEST_SUITE("test_suite2");
-    ts2->add(BOOST_TEST_CASE(&test_case3));
+    test_suite* ts2 = BOOST_TEST_SUITE("SUITE_Commands");
     ts2->add(BOOST_TEST_CASE(&test_case4));
 
     framework::master_test_suite().add(ts1);
